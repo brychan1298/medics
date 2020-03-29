e6ed1fdc2149588545b4ba6a04d19e5ceb9a63ac
michelles <mmichellesetiyanti@gmail.com>
2020-03-29 14:33:32 +0700

    mengerjakan tampilan about us

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -23,32 +23,29 @@
 				<center><img src="gambar/hem.webp" class="w-100 mt-5"></center>
 			</div>
 			<div class="col-sm-6 mt-5">
-				<div class="mt-5" style="font-size: 40px;text-align: right;">About Medics</div>
-				<div style="text-align: right;" class="mt-5 mb-5">Cum doctus civibus efficiantur in imperdiet deterruisset.</div>
-				<div style="text-align: right;">Lorem ipsum dolor sit amet, consectetur </div>
-				<div style="text-align: right;">adipiscing elit, sed do eiusmod tempor</div>
-				<div style="text-align: right;">incididunt ut labore et dolore magna aliqua.</div>
+				<div class="" style="font-size: 40px;text-align: right;margin-top: 100px;">About Medics</div>
+				<div style="text-align: right;" class="mt-3 mb-3">Medics is an application that we made<br> to facilitate us in consulting, buying drugs, in the health sector.</div>
+				<div style="text-align: right;">we created this application<br> so that all people can use our application. so that it is more instant, <br> practical and easy in all matters in the field of health</div>
 			</div>
 		</div>
 	</div>
 
 	<div class="mt-5">
-		<center><div style="font-size: 30px;">OUR FOUNDERS</div></center>
-		<center><img src="gambar/pembatas.png" style="width: 160px;"></center>
+		<center><div style="font-size: 40px;">OUR FOUNDERS</div><img src="gambar/pembatas.png" style="width: 160px;margin-bottom: 30px;"></center>
 	</div>
-	<div class="container mb-5 d-none d-lg-block">
+	<div class="container d-none d-lg-block" style="margin-bottom: 100px;">
 		<div class="row">
 			<div class="col-sm-4">
 				<img src="gambar/banks.jpg" class="w-100">
-				<center><div style="font-size: 20px;border: 1px solid;border-radius: 0px 0px 15px 15px;">Steve Marlins</div></center>
+				<center><div style="font-size: 20px;border: 1px solid black ;border-radius: 0px 0px 15px 15px;padding: 20px; color:#2E5EBE;font-weight: bold;">Liana Marlins</div></center>
 			</div>
 			<div class="col-sm-4">
 				<img src="gambar/flume.jpg" class="w-100">
-				<center><div style="font-size: 20px;border: 1px solid;border-radius: 0px 0px 15px 15px;">Steve Marlins</div></center>
+				<center><div style="font-size: 20px;border: 1px solid black ;border-radius: 0px 0px 15px 15px;padding: 20px; color:#2E5EBE;font-weight: bold;">Steve Marlins</div></center>
 			</div>
 			<div class="col-sm-4">
 				<img src="gambar/chet_faker_2.jpg" class="w-100">
-				<center><div style="font-size: 20px;border: 1px solid;border-radius: 0px 0px 15px 15px;">Steve Marlins</div></center>
+				<center><div style="font-size: 20px;border: 1px solid black ;border-radius: 0px 0px 15px 15px;padding: 20px; color:#2E5EBE;font-weight: bold;">Mark Marlins</div></center>
 			</div>
 		</div>
 	</div>
@@ -67,19 +64,19 @@
 	    <div class="carousel-item active">
 	      <div class="col-sm-4">
 				<img src="gambar/banks.jpg" class="w-100">
-				<center><div style="font-size: 20px;border: 1px solid;border-radius: 0px 0px 15px 15px;">Steve Marlins</div></center>
+				<center><div style="font-size: 20px;border: 1px solid;border-radius: 0px 0px 15px 15px;padding: 20px; color:#2E5EBE;font-weight: bold;">Liana Marlins</div></center>
 			</div>
 	    </div>
 	    <div class="carousel-item">
 	      <div class="col-sm-4">
 				<img src="gambar/flume.jpg" class="w-100">
-				<center><div style="font-size: 20px;border: 1px solid;border-radius: 0px 0px 15px 15px;">Steve Marlins</div></center>
+				<center><div style="font-size: 20px;border: 1px solid;border-radius: 0px 0px 15px 15px;padding: 20px; color:#2E5EBE;font-weight: bold;">Steve Marlins</div></center>
 			</div>
 	    </div>
 	    <div class="carousel-item">
 	      <div class="col-sm-4">
 				<img src="gambar/chet_faker_2.jpg" class="w-100">
-				<center><div style="font-size: 20px;border: 1px solid;border-radius: 0px 0px 15px 15px;">Steve Marlins</div></center>
+				<center><div style="font-size: 20px;border: 1px solid;border-radius: 0px 0px 15px 15px;padding: 20px; color:#2E5EBE;font-weight: bold;">Mark Marlins</div></center>
 			</div>
 	    </div>
 	  </div>
@@ -96,21 +93,24 @@
 
 
 	<div class="container mt-5">
-		<div class="row">
+		<div class="row" style="margin-bottom: 100px;">
 			<div class="col-sm-6">
 				<div class="col-sm-12">
-					<div style="border: 1px solid grey;margin-right: 280px;">
-						OUR VISION<br>Lorem Ipsum Color Sit Amet,<br>consectetur adipiscing elit, sed <br>do eiusmod tempo.
+					<div style="border: 1px solid grey;margin-right: 280px;padding: 5px;">
+						<p style="text-align: center;color:#2E5EBE;font-size: 18px;font-weight: bold;margin-top: 20px;">OUR VISION</p>
+						<p style="text-align: center;padding: 0px;margin-top: 0px;">Our vision is to make an application that has many benefits for everyone</p>
 					</div>
 				</div>
 				<div class="col-sm-12">
-					<div style="margin-left: 280px;border: 1px solid grey;">
-						OUR MISSION<br>Lorem Ipsum Color Sit Amet,<br>consectetur adipiscing elit, sed <br>do eiusmod tempo.
+					<div style="margin-left: 280px;border: 1px solid grey;padding: 5px;">
+						<p style="text-align: center;color:#2E5EBE;font-size: 18px;font-weight: bold;margin-top: 20px;">OUR MISSION</p>
+						<p style="text-align: center;padding: 0px;margin-top: 0px;">our mission is to make people happy and make it easier for everyone</p>
 					</div>
 				</div>
 				<div class="col-sm-12">
-					<div style="border: 1px solid grey;margin-right: 280px;">
-						OUR VALUES<br>Lorem Ipsum Color Sit Amet,<br>consectetur adipiscing elit, sed <br>do eiusmod tempo.
+					<div style="border: 1px solid grey;margin-right: 280px;padding: 5px;">
+						<p style="text-align: center;color:#2E5EBE;font-size: 18px;font-weight: bold;margin-top: 20px;">OUR VALUES</p>
+						<p style="text-align: center;padding: 0px;margin-top: 0px;">Our values is to form an application that can benefit everyone</p>
 					</div>
 				</div>
 			</div>
@@ -121,35 +121,35 @@
 	</div>
 
 	<div>
-		<center><div style="font-size: 30px;" class="mt-5">WHY CHOOSE MEDICS</div></center>
+		<center><div style="font-size: 40px;" class="mt-5">WHY CHOOSE MEDICS</div></center>
 		<center><img src="gambar/pembatas.png" style="width: 160px;"></center>
 	</div>
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-4">
-				<div style="border: 1px solid;"><center><img src="gambar/Asset 13.png" style="width: 100px;"><div style="color: blue;margin-top: 10px;">FIND DOCTORS</div>
-				<div class="mt-4">Usu habeo equidem sanctus no. <br>
- 						Suas summo id sed, erat errant <br>
- 						oporteat cu pri. In eum  <br>
-						omnes molestie.
+				<div style="border: 1px solid lightgrey;padding: 40px;"><center><img src="gambar/Asset 13.png" style="width: 200px;"><div style="text-align: center;color:#2E5EBE;font-size: 20px;font-weight: bold;margin-top: 20px;">FIND DOCTORS</div>
+				<div class="mt-2">
+					<center>
+						with this feature we can find a doctor in the position closest to us. so that we can consult or see a doctor at a particular hospital.
+					</center>
 				</div>
 			</center></div>
 			</div>
 			<div class="col-sm-4">
-				<div style="border: 1px solid;"><center><img src="gambar/Asset 15.png" style="width: 100px;"><div style="color: blue;margin-top: 10px;">SUPPORTING VIA CHAT</div>
-				<div class="mt-4">Usu habeo equidem sanctus no. <br>
- 						Suas summo id sed, erat errant <br>
- 						oporteat cu pri. In eum  <br>
-						omnes molestie.
+				<div style="border: 1px solid lightgrey;padding: 40px;"><center><img src="gambar/Asset 15.png" style="width: 200px;"><div style="text-align: center;color:#2E5EBE;font-size: 20px;font-weight: bold;margin-top: 20px;">SUPPORTING VIA CHAT</div>
+				<div class="mt-2">
+					<center>
+						this feature is an instant booking feature. this feature can make it easier for us to queue in the queue at the hospital, making it easier for us all.
+					</center>
 				</div>
 			</center></div>
 			</div>
 			<div class="col-sm-4">
-				<div style="border: 1px solid;"><center><img src="gambar/Asset 20.png" style="width: 110px;"><div style="color: blue;margin-top: 10px;">INSTANT BOOKING</div>
-				<div class="mt-4">Usu habeo equidem sanctus no. <br>
- 						Suas summo id sed, erat errant <br>
- 						oporteat cu pri. In eum  <br>
-						omnes molestie.
+				<div style="border: 1px solid lightgrey;padding: 40px;"><center><img src="gambar/Asset 20.png" style="width: 200px;"><div style="text-align: center;color:#2E5EBE;font-size: 20px;font-weight: bold;margin-top: 20px;">INSTANT BOOKING</div>
+				<div class="mt-2">
+					<center>
+						with this feature we can find a doctor in the position closest to us. so that we can consult or see a doctor at a particular hospital
+					</center>
 				</div>
 				</center></div>
 			</div>
