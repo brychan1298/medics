7a2eb68ce730da1ca868714d3af0aab041672b0a
michelles <mmichellesetiyanti@gmail.com>
2020-03-30 12:56:22 +0700

    shop

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -20,7 +20,7 @@
 				margin-top: 170px;
 			}
 			.img1{
-				width: 170px;
+				width: 150px;
 				text-align: center;
 			}
 			.resG1{
@@ -36,7 +36,7 @@
 		<img src="img/lolsssss.jpg" style="width: 100%;">
 	</div>
 
-	<div id="shop1">
+	<div id="shop1" style="font-size: 40px;">
 		<center>SHOP</center>
 	</div>
 
@@ -55,90 +55,143 @@
 	</div>
 
 	<div class="container">
-		<div class="row">
+		<div class="row" style="margin-bottom:35px;">
 			<div class="col-sm-4 col-6">
-				<div style="border:1px solid;border-radius: 25px 25px 0px 0px;">
-					<img src="img/obat1.png" class="img1">
-				</div>
-				<div style="color: white;background-color: blue;border-radius: 0px 0px 25px 25px; padding:15px;">
+				<center>
+					<div style="border:1px solid;border-radius: 25px 25px 0px 0px;border-bottom: 1px solid blue;">
+						<div style="background-color: #ADC5DD;border-radius: 50%; border:1px solid #ADC5DD;width: 180px;height: 180px;margin-top: 80px;z-index: 0;">
+						</div>
+							<img src="img/obat1.png" class="img1" style="width: 230px;margin-bottom: 0px;z-index: 1;margin-top: -250px;">
+							<p style="text-align: center;color:#2E5EBE;font-size: 20px;margin-top: -30px;font-weight: bold;">Sore Medication</p>
+							<p style="text-align: center;font-size: 18px;margin-top: -10px;">$50.00</p>
+					</div>
+				</center>
+				<div style="color: white;background-color: #2E5EBE;border:0px;border-radius: 0px 0px 25px 25px; padding:15px;font-weight: bold;">
 					<center>ADD TO CART</center>
 				</div>
 			</div>
-			
 			<div class="col-sm-4 col-6">
-				<div style="border:1px solid;border-radius: 25px 25px 0px 0px;">
-					<img src="img/obat6.png" class="img1">
-				</div>
-				<div style="color: white;background-color: blue;border-radius: 0px 0px 25px 25px; padding:15px;">
+				<center>
+					<div style="border:1px solid;border-radius: 25px 25px 0px 0px;border-bottom: 1px solid blue;">
+						<div style="background-color: #ADC5DD;border-radius: 50%; border:1px solid #ADC5DD;width: 180px;height: 180px;margin-top: 80px;z-index: 0;">
+						</div>
+							<img src="img/obat5.png" class="img1" style="width: 230px;margin-bottom: 0px;z-index: 1;margin-top: -250px;">
+							<p style="text-align: center;color:#2E5EBE;font-size: 20px;margin-top: -30px;font-weight: bold;">Sore Medication</p>
+							<p style="text-align: center;font-size: 18px;margin-top: -10px;">$50.00</p>
+					</div>
+				</center>
+				<div style="color: white;background-color: #2E5EBE;border:0px;border-radius: 0px 0px 25px 25px; padding:15px;font-weight: bold;">
 					<center>ADD TO CART</center>
 				</div>
 			</div>
-
-			<div class="col-sm-4 col-6  resG1" style="margin-bottom: 50px;">
-				<div style="border:1px solid;border-radius: 25px 25px 0px 0px;">
-					<img src="img/obat5.png" class="img1">
-				</div>
-				<div style="color: white;background-color: blue;border-radius: 0px 0px 25px 25px; padding:15px;">
+			<div class="col-sm-4 col-6">
+				<center>
+					<div style="border:1px solid;border-radius: 25px 25px 0px 0px;border-bottom: 1px solid blue;">
+						<div style="background-color: #ADC5DD;border-radius: 50%; border:1px solid #ADC5DD;width: 180px;height: 180px;margin-top: 80px;z-index: 0;">
+						</div>
+							<img src="img/obat6.png" class="img1" style="width: 230px;margin-bottom: 0px;z-index: 1;margin-top: -250px;">
+							<p style="text-align: center;color:#2E5EBE;font-size: 20px;margin-top: -30px;font-weight: bold;">Sore Medication</p>
+							<p style="text-align: center;font-size: 18px;margin-top: -10px;">$50.00</p>
+					</div>
+				</center>
+				<div style="color: white;background-color: #2E5EBE;border:0px;border-radius: 0px 0px 25px 25px; padding:15px;font-weight: bold;">
 					<center>ADD TO CART</center>
 				</div>
 			</div>
+		</div>
+	</div>
 
-
-			<div class="col-sm-4 col-6 resG1">
-				<div style="border:1px solid;border-radius: 25px 25px 0px 0px;">
-					<img src="img/obat1.png" class="img1">
-				</div>
-				<div style="color: white;background-color: blue;border-radius: 0px 0px 25px 25px; padding:15px;">
+		<div class="container">
+		<div class="row" style="margin-bottom:35px;">
+			<div class="col-sm-4 col-6">
+				<center>
+					<div style="border:1px solid;border-radius: 25px 25px 0px 0px;border-bottom: 1px solid blue;">
+						<div style="background-color: #ADC5DD;border-radius: 50%; border:1px solid #ADC5DD;width: 180px;height: 180px;margin-top: 80px;z-index: 0;">
+						</div>
+							<img src="img/obat1.png" class="img1" style="width: 230px;margin-bottom: 0px;z-index: 1;margin-top: -250px;">
+							<p style="text-align: center;color:#2E5EBE;font-size: 20px;margin-top: -30px;font-weight: bold;">Sore Medication</p>
+							<p style="text-align: center;font-size: 18px;margin-top: -10px;">$50.00</p>
+					</div>
+				</center>
+				<div style="color: white;background-color: #2E5EBE;border:0px;border-radius: 0px 0px 25px 25px; padding:15px;font-weight: bold;">
 					<center>ADD TO CART</center>
 				</div>
 			</div>
-			
-			<div class="col-sm-4 d-none d-lg-block">
-				<div style="border:1px solid;border-radius: 25px 25px 0px 0px;">
-					<img src="img/obat6.png">
-				</div>
-				<div style="color: white;background-color: blue;border-radius: 0px 0px 25px 25px; padding:15px;">
+			<div class="col-sm-4 col-6">
+				<center>
+					<div style="border:1px solid;border-radius: 25px 25px 0px 0px;border-bottom: 1px solid blue;">
+						<div style="background-color: #ADC5DD;border-radius: 50%; border:1px solid #ADC5DD;width: 180px;height: 180px;margin-top: 80px;z-index: 0;">
+						</div>
+							<img src="img/obat5.png" class="img1" style="width: 230px;margin-bottom: 0px;z-index: 1;margin-top: -250px;">
+							<p style="text-align: center;color:#2E5EBE;font-size: 20px;margin-top: -30px;font-weight: bold;">Sore Medication</p>
+							<p style="text-align: center;font-size: 18px;margin-top: -10px;">$50.00</p>
+					</div>
+				</center>
+				<div style="color: white;background-color: #2E5EBE;border:0px;border-radius: 0px 0px 25px 25px; padding:15px;font-weight: bold;">
 					<center>ADD TO CART</center>
 				</div>
 			</div>
-
-			<div class="col-sm-4 d-none d-lg-block" style="margin-bottom: 50px;">
-				<div style="border:1px solid;border-radius: 25px 25px 0px 0px;">
-					<img src="img/obat5.png">
-				</div>
-				<div style="color: white;background-color: blue;border-radius: 0px 0px 25px 25px; padding:15px;">
+			<div class="col-sm-4 col-6">
+				<center>
+					<div style="border:1px solid;border-radius: 25px 25px 0px 0px;border-bottom: 1px solid blue;">
+						<div style="background-color: #ADC5DD;border-radius: 50%; border:1px solid #ADC5DD;width: 180px;height: 180px;margin-top: 80px;z-index: 0;">
+						</div>
+							<img src="img/obat6.png" class="img1" style="width: 230px;margin-bottom: 0px;z-index: 1;margin-top: -250px;">
+							<p style="text-align: center;color:#2E5EBE;font-size: 20px;margin-top: -30px;font-weight: bold;">Sore Medication</p>
+							<p style="text-align: center;font-size: 18px;margin-top: -10px;">$50.00</p>
+					</div>
+				</center>
+				<div style="color: white;background-color: #2E5EBE;border:0px;border-radius: 0px 0px 25px 25px; padding:15px;font-weight: bold;">
 					<center>ADD TO CART</center>
 				</div>
 			</div>
+		</div>
+	</div>
 
-
-			<div class="col-sm-4 d-none d-lg-block">
-				<div style="border:1px solid;border-radius: 25px 25px 0px 0px;">
-					<img src="img/obat5.png">
-				</div>
-				<div style="color: white;background-color: blue;border-radius: 0px 0px 25px 25px; padding:15px;">
+		<div class="container">
+		<div class="row" style="margin-bottom:35px;">
+			<div class="col-sm-4 col-6">
+				<center>
+					<div style="border:1px solid;border-radius: 25px 25px 0px 0px;border-bottom: 1px solid blue;">
+						<div style="background-color: #ADC5DD;border-radius: 50%; border:1px solid #ADC5DD;width: 180px;height: 180px;margin-top: 80px;z-index: 0;">
+						</div>
+							<img src="img/obat1.png" class="img1" style="width: 230px;margin-bottom: 0px;z-index: 1;margin-top: -250px;">
+							<p style="text-align: center;color:#2E5EBE;font-size: 20px;margin-top: -30px;font-weight: bold;">Sore Medication</p>
+							<p style="text-align: center;font-size: 18px;margin-top: -10px;">$50.00</p>
+					</div>
+				</center>
+				<div style="color: white;background-color: #2E5EBE;border:0px;border-radius: 0px 0px 25px 25px; padding:15px;font-weight: bold;">
 					<center>ADD TO CART</center>
 				</div>
 			</div>
-			
-			<div class="col-sm-4 d-none d-lg-block" style="margin-bottom: 100px;">
-				<div style="border:1px solid;border-radius: 25px 25px 0px 0px;">
-					<img src="img/obat1.png">
-				</div>
-				<div style="color: white;background-color: blue;border-radius: 0px 0px 25px 25px; padding:15px;">
+			<div class="col-sm-4 col-6">
+				<center>
+					<div style="border:1px solid;border-radius: 25px 25px 0px 0px;border-bottom: 1px solid blue;">
+						<div style="background-color: #ADC5DD;border-radius: 50%; border:1px solid #ADC5DD;width: 180px;height: 180px;margin-top: 80px;z-index: 0;">
+						</div>
+							<img src="img/obat5.png" class="img1" style="width: 230px;margin-bottom: 0px;z-index: 1;margin-top: -250px;">
+							<p style="text-align: center;color:#2E5EBE;font-size: 20px;margin-top: -30px;font-weight: bold;">Sore Medication</p>
+							<p style="text-align: center;font-size: 18px;margin-top: -10px;">$50.00</p>
+					</div>
+				</center>
+				<div style="color: white;background-color: #2E5EBE;border:0px;border-radius: 0px 0px 25px 25px; padding:15px;font-weight: bold;">
 					<center>ADD TO CART</center>
 				</div>
 			</div>
-
-			<div class="col-sm-4 d-none d-lg-block">
-				<div style="border:1px solid;border-radius: 25px 25px 0px 0px;">
-					<img src="img/obat6.png">
-				</div>
-				<div style="color: white;background-color: blue;border-radius: 0px 0px 25px 25px; padding:15px;">
+			<div class="col-sm-4 col-6">
+				<center>
+					<div style="border:1px solid;border-radius: 25px 25px 0px 0px;border-bottom: 1px solid blue;">
+						<div style="background-color: #ADC5DD;border-radius: 50%; border:1px solid #ADC5DD;width: 180px;height: 180px;margin-top: 80px;z-index: 0;">
+						</div>
+							<img src="img/obat6.png" class="img1" style="width: 230px;margin-bottom: 0px;z-index: 1;margin-top: -250px;">
+							<p style="text-align: center;color:#2E5EBE;font-size: 20px;margin-top: -30px;font-weight: bold;">Sore Medication</p>
+							<p style="text-align: center;font-size: 18px;margin-top: -10px;">$50.00</p>
+					</div>
+				</center>
+				<div style="color: white;background-color: #2E5EBE;border:0px;border-radius: 0px 0px 25px 25px; padding:15px;font-weight: bold;">
 					<center>ADD TO CART</center>
 				</div>
 			</div>
-
 		</div>
 	</div>
 
